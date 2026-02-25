cfd05fa4a9e152f45f8ed7c9008d3661d037719b
Renan "Firehawk" Lazarotto <renanlazarotto@gmail.com>
2026-02-25 14:09:44 -0300

    chore: drop STFS entirely

--- src/lib/helpers/stfs.ts
+++
@@ -1,9 +0,0 @@
-export class STFS {
-	static extractFromCON(conData: Uint8Array) {
-		// TODO
-	}
-
-	static validateCON(data: Uint8Array): boolean {
-		return new TextDecoder().decode(data.slice(0, 4)) === 'CON ';
-	}
-}
