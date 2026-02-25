54061828f2e7ca86a87597f2457ed275e45b9658
Renan "Firehawk" Lazarotto <renanlazarotto@gmail.com>
2026-02-25 14:13:33 -0300

    chore: auto-open on browser and change port

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit(), devtoolsJson()] });
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	server: {
+		open: true,
+		port: 4689, // GOTY on a T9 keypad. Lame, I know.
+	},
+});
